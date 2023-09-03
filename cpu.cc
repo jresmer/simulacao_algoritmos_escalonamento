1e882df2dea6a1dbba7ac31efa8a680eca89ea89
jresmer <jpresmer@gmail.com>
2023-09-03 20:03:22 -0300

    first commit

---
+++ cpu.cc
@@ -0,0 +1,42 @@
+#include "utils.h"
+
+using namespace utils;
+
+CPU::CPU() {
+    
+}
+
+CPU::CPU(Process * processes) {
+    sp = 0;
+    pc = 0;
+    st = 0;
+    gp = new long int[6];
+    process = processes;
+}
+
+CPU::~CPU() {
+    delete[] gp;
+    delete process;
+}
+
+void CPU::run() {
+    
+}
+
+Process * CPU::get_process() {
+    return process;
+}
+
+void CPU::set_process(Process * process_) {
+    process = process_;
+}
+
+
+context CPU::get_context() {
+    context contexto;
+    contexto.sp = sp;
+    contexto.pc = pc;
+    contexto.status = st;
+    contexto.gp = gp;
+    return contexto;
+}
---
+++ scheduler.cc
@@ -0,0 +1,49 @@
+#include "utils.h"
+
+
+using namespace utils;
+
+Scheduler::Scheduler() {}
+
+explicit Scheduler::Scheduler() {
+}
+
+Scheduler::~Scheduler() {
+
+}
+
+bool Scheduler::done() {
+    for (int i; i < 6; i++) {
+        if (processes[i] == nullptr)
+            return false;
+        else if (processes[i] -> get_state() != Finished)
+            return false;
+    }
+    return true;
+}
+
+void Scheduler::fcfs() {
+    while (1) {
+
+        if (done())
+            break;
+
+        
+    }
+}
+
+void Scheduler::sjf () {
+
+}
+
+void Scheduler::preemptive_priority() {
+
+}
+
+void Scheduler::non_preemptive_priority() {
+
+}
+
+void Scheduler::round_robin() {
+
+}
