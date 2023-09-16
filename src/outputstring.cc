fa31d86f6dfafb5a61c46d91825cd354cdf8a492
jresmer <112180846+jresmer@users.noreply.github.com>
2023-09-16 11:56:25 -0300

    Update outputstring.cc

--- src/outputstring.cc
+++ src/outputstring.cc
@@ -53,5 +53,17 @@ void OutputString::print_line(vector<Process*> &procecess, int time_) { // atual
 }
 
 void OutputString::print_final(vector<Process*> &procecess) {
+    int n_processes = processes.size()
+    int turnaround_time = 0;
+    int avg_wait_time = 0;
     // Implementar
+    for (int i = 0; i < n_processes; i++) {
+        turnaround_time += processes[i] -> get_turnaround_time();
+        avg_wait_time += processes[i] -> get_wait_time();
+        cout << "P" << processes[i] -> get_pid() << ": " << processes[i] -> get_turnaround_time() << endl;
+    }
+    avg_wait_time = avg_wait_time / n_processes;
+    cout << "Average wait time : " << avg_wait_time << endl;
+    turnaround_time = turnaround_time / n_processes;
+    cout << "Average Turnaround Time" << turnaround_time << endl;
 }
